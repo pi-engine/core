7fd1a81b20b1d7d5055ba913da69158b32d2f2f3
voltan <hossein@azizabadi.com>
2025-12-23 12:59:38 +0330

    Update headers

--- src/Security/Response/Headers.php
+++ src/Security/Response/Headers.php
@@ -52,10 +52,11 @@ class Headers implements ResponseSecurityInterface
             ->withHeader('Cross-Origin-Embedder-Policy', 'require-corp')
             ->withHeader('Cross-Origin-Opener-Policy', 'same-origin')
             ->withHeader('Cross-Origin-Resource-Policy', 'same-origin')
-            //->withHeader('Access-Control-Allow-Origin', $this->config['cors']['allowed_origins'])
-            //->withHeader('Access-Control-Allow-Methods', $this->config['cors']['allowed_methods'])
-            //->withHeader('Access-Control-Allow-Headers', $this->config['cors']['allowed_headers'])
-            //->withHeader('Access-Control-Max-Age', '3600')
+            ->withHeader('Access-Control-Allow-Origin', $this->config['cors']['allowed_origins'])
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Allow-Methods', $this->config['cors']['allowed_methods'])
+            ->withHeader('Access-Control-Allow-Headers', $this->config['cors']['allowed_headers'])
+            ->withHeader('Access-Control-Max-Age', '3600')
 
             // Expect-CT
             ->withHeader('Expect-CT', 'max-age=86400, enforce')
